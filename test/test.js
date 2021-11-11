da33d019e11406c229bbf03ba2fd0c541a38aa3b
Andre Vallestero <andrevallestero@gmail.com>
2021-11-11 14:36:07 -0500

    Change tabs to spaces

--- test/test.js
+++ test/test.js
@@ -1,22 +1,22 @@
 const {
-	arLocal,
-	arweave,
-	setupKoiiNode,
-	mineBlock
+  arLocal,
+  arweave,
+  setupKoiiNode,
+  mineBlock
 } = require("./helper");
 
 beforeAll(async () => {
-	await arLocal.start();
-	await setupKoiiNode();
+  await arLocal.start();
+  await setupKoiiNode();
 });
 
 afterAll(async () => {
-	await arLocal.stop();
+  await arLocal.stop();
 });
 
 test("Block height is 0", async () => {
-	const networkInfo = await arweave.network.getInfo();
-	height = networkInfo.height;
-	expect(height).toBeGreaterThan(10);
+  const networkInfo = await arweave.network.getInfo();
+  height = networkInfo.height;
+  expect(height).toBeGreaterThan(10);
 });
 
